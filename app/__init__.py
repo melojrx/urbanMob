8790bb87fb7621cc2c01e6b2ccd9d0b9d71d4edc
Cyro Sabóia <cyrosaboia@gmail.com>
2022-07-12 19:21:07 -0300

    Tentando comunicação entre controllers

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost/postgres'
 app.config['SECRET_KEY'] = 'secret'
@@ -9,4 +10,9 @@ app.config['SECRET_KEY'] = 'secret'
 login_manager = LoginManager(app)
 db = SQLAlchemy(app)
 
-from app.controllers import user_controller
+
+from app.controllers import userController
+from app.controllers import homeController 
+
+
+app.register_blueprint(homeController.listUsers_bp, url_prefix='/listUsers')
--- app/controllers/userController.py
+++ app/controllers/userController.py
@@ -1,14 +1,11 @@
 from flask import render_template, request, redirect, url_for
 from flask_login import login_user, logout_user
 from app import app, db
-from app.models import user_models
-from ..models.user_models import User 
+from app.models.userModels import User 
 
 @app.route('/')
 def home():
-    # return redirect(url_for('login')) 
     return redirect('/login')
-    # return render_template('login.html')
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -36,13 +33,11 @@ def login():
         if not user or not user.verify_password(pwd):
             return redirect(url_for('login'))        
 
-        print('NNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN')
         login_user(user)
-        print('OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO')
-        # return redirect(url_for('home'))
-        return render_template('home.html')
+        return redirect(url_for('/listUsers')) 
+        #return redirect("/listUsers")
+        #return render_template('home.html')
     else:
-        print('PPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPPP')
         return render_template('login.html')
 
 
